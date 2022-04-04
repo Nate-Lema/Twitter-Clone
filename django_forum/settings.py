f7156ebc3565ce566c0a0879e68d1dd5177b5ae0
Nathanael Lema <Natnaellema@iclous.com>
2022-04-03 20:24:00 -0400

    {comment}

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import cloudinary 
+import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ cloudinary.config(
 )
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
